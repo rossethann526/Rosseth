c6e66f08e8cf48d2abadf0d070f6c00e6fd5aeb7
rosseth.github.io <rosseth_021346@binalatongan.edu.ph>
2024-06-08 13:11:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+const btnLike1 = document.getElementById("btnLike1")
+const count1 = document.getElementById("count1")
+
+function countLike1() {
+  let likeCounts = parseInt(count1.value) + 1
+  count1.textContent = likeCounts.toString()
+}
+btnLike1.addEventListener("click", countLike1)
+const btnLike2 = document.getElementById("btnLike2")
+const count2 = document.getElementById("count2")
+
+function countLike2() {
+  let likeCounts = parseInt(count2.value) + 1
+  count2.textContent = likeCounts.toString()
+}
+btnLike2.addEventListener("click", countLike2)
+
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+
+function submitComment(){
+  commentbox.textContent += comment.value.toString()+ "\n"
+}
+submit.addEventListener("click",submitComment)
